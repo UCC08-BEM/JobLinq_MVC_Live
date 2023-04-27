11e2b5644d541409af264fd4779cf30dfa6eaf1e
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-04-27 14:05:54 +0300

    Add project files.

---
+++ JobLinq_MVC_Live/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobLinq_MVC_Live.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
