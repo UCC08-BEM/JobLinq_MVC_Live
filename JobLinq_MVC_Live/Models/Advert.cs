3405e3fd621307fb7e78a0f780bf9dac75b130c3
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-05-02 13:52:03 +0300

    Some changes

--- JobLinq_MVC_Live/Models/Advert.cs
+++ JobLinq_MVC_Live/Models/Advert.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace JobLinq_MVC_Live.Models
 {
     public partial class Advert
     {
         public int AdvertId { get; set; }
+
+        [DisplayName("Şirket ID")]
         public int? CompanyId { get; set; }
+        [DisplayName("İlan Başlığı")]
         public string? AdvertTitle { get; set; }
+        [DisplayName("İlan Detayı")]
         public string? AdvertDetail { get; set; }
+        [DisplayName("Şehir")]
         public byte? CityId { get; set; }
+        [DisplayName("İş Tipi")]
         public string? JobType { get; set; }
     }
 }
--- JobLinq_MVC_Live/Models/City.cs
+++ JobLinq_MVC_Live/Models/City.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace JobLinq_MVC_Live.Models
 {
     public partial class City
     {
         public byte CityId { get; set; }
+
+        [DisplayName("Şehir Adı")]
         public string? CityName { get; set; }
     }
 }
