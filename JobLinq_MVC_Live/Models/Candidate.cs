b05993205f2d00cb40cf95c7054b1dadcfdcd1d6
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-05-03 10:58:33 +0300

    Some changes

--- JobLinq_MVC_Live/Models/Candidate.cs
+++ JobLinq_MVC_Live/Models/Candidate.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace JobLinq_MVC_Live.Models
 {
     public partial class Candidate
     {
         public int CandidateId { get; set; }
+
+        [DisplayName("User ID")]
         public int? UserId { get; set; }
+
+        [DisplayName("Adı")]
         public string? Fname { get; set; }
+        
+        [DisplayName("Soyadı")]
         public string? Lname { get; set; }
+        
+        [DisplayName("Doğum Tarihi")]
         public DateTime? BirthDate { get; set; }
+        
+        [DisplayName("Şehir")]
         public byte? CityId { get; set; }
+        
+        [DisplayName("Telefon No")]
         public string? Gsmno { get; set; }
     }
 }
--- JobLinq_MVC_Live/Models/Company.cs
+++ JobLinq_MVC_Live/Models/Company.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace JobLinq_MVC_Live.Models
 {
     public partial class Company
     {
         public int CompanyId { get; set; }
+
+        [DisplayName("User ID")]
         public int? UserId { get; set; }
+
+        [DisplayName("Şirket Adı")]
         public string? Cname { get; set; }
+
+        [DisplayName("Sektör No")]
         public byte? SectorId { get; set; }
+
+        [DisplayName("Şirket Adres")]
         public string? Cadress { get; set; }
+
+        [DisplayName("Şehir")]
         public byte? CityId { get; set; }
     }
 }
